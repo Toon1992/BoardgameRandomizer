61830ad8af2c28fbf237259582d4dac85e978c8d
toon1992 <toon.detrue.v7134@student.hogent.be>
2017-01-24 15:52:50 +0100

    first commit

---
+++ app/src/main/java/com/example/toon/boardgamerandomizer/Model/Boardgame.java
@@ -0,0 +1,81 @@
+package com.example.toon.boardgamerandomizer.Model;
+
+/**
+ * Created by Toon on 24/01/2017.
+ */
+
+public class Boardgame {
+    private int id;
+    private String name;
+    private String thumbnailUrl;
+    private int minPlayer;
+    private int maxPlayer;
+    private int playingTime;
+    private boolean owned;
+
+    public Boardgame(int id, String name, String thumbnailUrl, int minPlayer, int maxPlayer, int playingTime, boolean owned) {
+        this.id = id;
+        this.name = name;
+        this.thumbnailUrl = thumbnailUrl;
+        this.minPlayer = minPlayer;
+        this.maxPlayer = maxPlayer;
+        this.playingTime = playingTime;
+        this.owned = owned;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public int getMinPlayer() {
+        return minPlayer;
+    }
+
+    public void setMinPlayer(int minPlayer) {
+        this.minPlayer = minPlayer;
+    }
+
+    public int getMaxPlayer() {
+        return maxPlayer;
+    }
+
+    public void setMaxPlayer(int maxPlayer) {
+        this.maxPlayer = maxPlayer;
+    }
+
+    public int getPlayingTime() {
+        return playingTime;
+    }
+
+    public void setPlayingTime(int playingTime) {
+        this.playingTime = playingTime;
+    }
+
+    public boolean isOwned() {
+        return owned;
+    }
+
+    public void setOwned(boolean owned) {
+        this.owned = owned;
+    }
+}
---
+++ app/src/main/java/com/example/toon/boardgamerandomizer/Model/Root.java
@@ -0,0 +1,13 @@
+package com.example.toon.boardgamerandomizer.Model;
+
+/**
+ * Created by Toon on 24/01/2017.
+ */
+
+public class Root {
+    private Boardgame[] boardgames;
+
+    public Root(Boardgame[] boardgames) {
+        this.boardgames = boardgames;
+    }
+}
